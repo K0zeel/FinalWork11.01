f4902909dbdad597dc5225e308262ab64885ee1d
kokos <kokos@DESKTOP-4K729EO>
2024-12-20 03:43:41 +0300

    Добавьте файлы проекта.

---
+++ FragrantWorld/Pages/MainShopPage.xaml.cs
@@ -0,0 +1,169 @@
+﻿using ServiceLayer.Models;
+using ServiceLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FragrantWorld.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainShopPage.xaml
+    /// </summary>
+    public partial class MainShopPage : Page
+    {
+        private readonly ProductService _productService = new();
+
+        public MainShopPage(User user = null)
+        {
+            InitializeComponent();
+            if (user == null)
+            {
+                UserTextBlock.Text = "Гость";
+            }
+            else
+            {
+                UserTextBlock.Text = user.Name;
+            }
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadProductsAsync();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.CurrentFrame.CanGoBack)
+                App.CurrentFrame.GoBack();
+        }
+
+        private async Task LoadProductsAsync()
+        {
+            try
+            {
+                var product = await _productService.GetProductsAsync();
+                foreach (Product productItem in product)
+                    CreatProductConteiner(productItem);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CreatProductConteiner(Product productItem)
+        {
+            try
+            {
+                StackPanel panel = new()
+                {
+                    Width = 630,
+                    Margin = new Thickness(15),
+                    Background = new SolidColorBrush(Color.FromRgb(255, 204, 153)),
+
+                };
+
+                Border topBorder = new()
+                {
+                    BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                    BorderThickness = new Thickness(1),
+                    CornerRadius = new CornerRadius(5)
+                };
+                Border botBorder = new()
+                {
+                    BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                    BorderThickness = new Thickness(1),
+                    CornerRadius = new CornerRadius(5)
+                };
+
+                Grid grid = new() { };
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                TextBlock ProductTextBlock = new TextBlock()
+                {
+                    Text = productItem.Name,
+                    FontWeight = FontWeights.Bold
+                };
+                Grid.SetRow(ProductTextBlock, 0);
+                Grid.SetColumn(ProductTextBlock, 0);
+                grid.Children.Add(ProductTextBlock);
+
+                TextBlock DescriptionTextBlock = new TextBlock
+                {
+                    Text = productItem.Description
+                };
+                Grid.SetRow(DescriptionTextBlock, 1);
+                Grid.SetColumn(DescriptionTextBlock, 0);
+                grid.Children.Add(DescriptionTextBlock);
+
+                TextBlock ManufacturerTextBlock = new TextBlock
+                {
+                    Text = $"Производитель: {productItem.Manufacturer}",
+                };
+                Grid.SetRow(ManufacturerTextBlock, 2);
+                Grid.SetColumn(ManufacturerTextBlock, 0);
+                grid.Children.Add(ManufacturerTextBlock);
+
+                TextBlock PriceTextBlock = new TextBlock
+                {
+                    Text = $"Цена: {productItem.Cost}",
+                };
+                Grid.SetRow(PriceTextBlock, 3);
+                Grid.SetColumn(PriceTextBlock, 0);
+                grid.Children.Add(PriceTextBlock);
+
+                Button OrderButton = new Button
+                {
+                    Content = "Заказать",
+                    HorizontalAlignment = HorizontalAlignment.Right,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Background = new SolidColorBrush(Color.FromRgb(204, 102, 0)),
+                    BorderBrush = new SolidColorBrush(Color.FromRgb(204, 102, 0)),
+                };
+                Grid.SetRow(OrderButton, 3);
+                Grid.SetColumn(OrderButton, 1);
+                grid.Children.Add(OrderButton);
+
+                panel.Children.Add(topBorder);
+                panel.Children.Add(grid);
+                panel.Children.Add(botBorder);
+                ProductStackPanel.Children.Add(panel);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void sortComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = ((ComboBoxItem)sortComboBox.SelectedItem).Content.ToString();
+            if (selectedItem == "Цена по возрастанию")
+            {
+                
+            }
+            else if (selectedItem == "Цена по убыванию")
+            {
+
+            }
+        }
+    }
+}
---
+++ ServiceLayer/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceLayer.Data;
+using ServiceLayer.Models;
+
+namespace ServiceLayer.Services
+{
+    public class UserService
+    {
+        private readonly ShopContext _context = new();
+
+        public async Task<bool> IsUserExist(string login, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Login == login && u.Password == password);
+            if (user != null)
+                return true;
+            else
+                return false;
+        }
+        public async Task<User?> GetUserByLogin(string login)
+            => await _context.Users.FirstOrDefaultAsync(u => u.Login == login);
+    }
+}
